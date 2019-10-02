55f1e9367ad2e8b460b2899da87e43b6f8340901
Tyler Schaffer <tpschaffer@gmail.com>
2019-10-01 22:38:31 -0600

    Make customer index more testing more thorough

--- spec/requests/api/v1/customers_request_spec.rb
+++ spec/requests/api/v1/customers_request_spec.rb
@@ -1,11 +1,14 @@
 require 'rails_helper'
 
 describe "Customers API" do
-  it "sends a list of customers" do
+  it "provides a customer index" do
     create_list(:customer, 3)
 
     get '/api/v1/customers'
 
     expect(response).to be_successful
+
+    data = JSON.parse(response.body)
+    expect(data["data"].count).to eq 3
   end
 end
