23824a603808f03957e60925223b53854267ffc5
Tyler Schaffer <tpschaffer@gmail.com>
2019-10-02 22:06:21 -0600

    Create find action for invoices

---
+++ app/controllers/api/v1/invoices/find_controller.rb
@@ -0,0 +1,12 @@
+class Api::V1::Invoices::FindController < ApplicationController
+
+  def find
+    render json: InvoiceSerializer.new(Invoice.find_by(invoice_params))
+  end
+
+  private
+
+  def invoice_params
+    params.permit(:id,:customer_id,:merchant_id,:status,:created_at,:updated_at)
+  end
+end
