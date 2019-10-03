e4542329bf927b1090da9a9c065bed171fdbc5de
Tyler Schaffer <tpschaffer@gmail.com>
2019-10-03 09:54:23 -0600

    Update table strings to be case insensitive

--- db/migrate/20191001190243_create_items.rb
+++ db/migrate/20191001190243_create_items.rb
@@ -1,8 +1,10 @@
 class CreateItems < ActiveRecord::Migration[5.1]
   def change
+    enable_extension 'citext'
+
     create_table :items do |t|
-      t.string :name
-      t.string :description
+      t.citext :name
+      t.citext :description
       t.integer :unit_price
       t.references :merchant, foreign_key: true
 
