09493eeb381bcd69ad395a753bbf9fb1f96bb7a1
Tyler Schaffer <tpschaffer@gmail.com>
2019-10-03 16:47:35 -0600

    Create index action for all invoice_items associated with an item

--- config/routes.rb
+++ config/routes.rb
@@ -31,6 +31,7 @@ Rails.application.routes.draw do
         get '/find', to: 'find#show'
         get '/find_all', to: 'find_all#index'
         get '/random', to: 'random#show'
+        get '/:id/invoice_items', to: 'invoice_items#index'
       end
 
       namespace :merchants do
