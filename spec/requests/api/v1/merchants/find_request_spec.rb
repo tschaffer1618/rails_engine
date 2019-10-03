05b1769800984d2561a6865256bbbf46f121ee70
Tyler Schaffer <tpschaffer@gmail.com>
2019-10-03 11:59:37 -0600

    Create index action for finding all merchants by attribute

--- spec/requests/api/v1/merchants/find_request_spec.rb
+++ spec/requests/api/v1/merchants/find_request_spec.rb
@@ -1,31 +1,6 @@
 require 'rails_helper'
 
 describe "Merchants API" do
-  it "provides a merchant index" do
-    create_list(:merchant, 3)
-
-    get '/api/v1/merchants'
-
-    expect(response).to be_successful
-
-    data = JSON.parse(response.body)
-    expect(data["data"].count).to eq 3
-  end
-
-  it "provides data for a single customer" do
-    merchant = create(:merchant)
-
-    get "/api/v1/merchants/#{merchant.id}"
-
-    expect(response).to be_successful
-
-    data = JSON.parse(response.body)
-    expect(data["data"]["id"]).to eq "#{merchant.id}"
-    expect(data["data"]["type"]).to eq "merchant"
-    expect(data["data"]["attributes"]["id"]).to eq merchant.id
-    expect(data["data"]["attributes"]["name"]).to eq merchant.name
-  end
-
   describe "finds the first merchant" do
     before(:each) do
       create_list(:merchant, 3)
