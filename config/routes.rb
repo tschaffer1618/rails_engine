1c58f3cd178502fe43214172988c3f5aa34ad8f1
Tyler Schaffer <tpschaffer@gmail.com>
2019-10-02 21:48:40 -0600

    Create find action for customers

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,30 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
+      namespace :customers do
+        get '/find', to: 'find#find'
+      end
+
+      namespace :invoice_items do
+        get '/find', to: 'find#find'
+      end
+
+      namespace :invoices do
+        get '/find', to: 'find#find'
+      end
+
+      namespace :items do
+        get '/find', to: 'find#find'
+      end
+
+      namespace :merchants do
+        get '/find', to: 'find#find'
+      end
+
+      namespace :transactions do
+        get '/find', to: 'find#find'
+      end
+
       resources :customers, only: [:index, :show]
       resources :invoice_items, only: [:index, :show]
       resources :invoices, only: [:index, :show]
