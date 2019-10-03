ac446f1bb94920b9506bc0761ff5598c393c37ba
Tyler Schaffer <tpschaffer@gmail.com>
2019-10-02 21:10:33 -0600

    Add created_at and updated_at to serializers

--- app/serializers/invoice_item_serializer.rb
+++ app/serializers/invoice_item_serializer.rb
@@ -1,4 +1,4 @@
 class InvoiceItemSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :id, :item_id, :invoice_id, :quantity, :unit_price
+  attributes :id, :item_id, :invoice_id, :quantity, :unit_price, :created_at, :updated_at
 end
