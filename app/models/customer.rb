ace4f06b64bc140a7961edd3ba6715aa9865870f
Tyler Schaffer <tpschaffer@gmail.com>
2019-10-05 13:08:02 -0600

    Build skeleton for a customer's favorite merchant

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,13 @@ class Customer < ApplicationRecord
   has_many :transactions, through: :invoices
 
   validates_presence_of :first_name, :last_name
+
+  def favorite_merchant
+    self.merchants.joins(:invoices, :transactions)
+      .where(transaction: {result: :success})
+      .group(:id)
+      .select('merchants.*, count(transactions.id) AS successes')
+      .order('successes desc')
+      .limit(1)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
         get '/random', to: 'random#show'
         get '/:id/invoices', to: 'invoices#index'
         get '/:id/transactions', to: 'transactions#index'
+        get '/:id/favorite_merchant', to: 'favorite_merchant#show'
       end
 
       namespace :invoice_items do
